d7d7e22aa786f8601a0a64fe8192e7f174aa5624
Jesse Burlison <36417798+JesseBurlison@users.noreply.github.com>
2023-05-01 21:08:49 -0400

    Added website url

--- app/src/main/java/com/example/brewbuddyproject/DetailsActivity.kt
+++ app/src/main/java/com/example/brewbuddyproject/DetailsActivity.kt
@@ -1,8 +1,12 @@
 package com.example.brewbuddyproject
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 
 class DetailsActivity : AppCompatActivity() {
@@ -23,12 +27,16 @@ class DetailsActivity : AppCompatActivity() {
         Log.d(TAG, "breweryStreet: $brewStreet")
         Log.d(TAG, "breweryCity: $brewCity")
         Log.d(TAG, "breweryPhone: $brewPhone")
-        Log.d(TAG, "breweryWebsite: $brewWebsite")
 
         findViewById<TextView>(R.id.details_brewery_name).text = brewName.toString()
-        findViewById<TextView>(R.id.details_brewery_addresss).text = brewStreet.toString() + ", " + brewCity.toString() + ", " + "State will go here"
+        findViewById<TextView>(R.id.details_brewery_addresss).text = brewStreet.toString() + ", " + brewCity.toString()
         findViewById<TextView>(R.id.details_brewery_phone_number).text = "Phone: " + brewPhone.toString()
 
 
+    }
+
+    fun websiteButton(view: View){
+        val i = Intent(Intent.ACTION_VIEW, Uri.parse(intent.getStringExtra("brewWebsite")))
+        startActivity(i)
     }
 }
